230424fb48e764f94bae8178289647768e1c1e29
Chander Parkash <chanderparkash179@gmail.com>
2023-12-13 00:47:31 +0500

    http methods

--- modules/http/api.js
+++ modules/http/api.js
@@ -2,32 +2,42 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+/*
+==> HTTP METHODS <==
+
+==> GET     : it's used to get the data from server 
+==> POST    : it's used to send data to server
+==> PUT     : it's used to update data on server
+==> DELETE  : it's used to delete the data from server
+==> PATCH   : it's used to update a particular field of object on server
+*/
+
 http
   .createServer((req, res) => {
     let date = new Date();
     let my_url = url.parse(req.url, true);
 
-    const log = `${date.getTime()} : "${my_url.pathname}" : User Logged\n`;
+    const log = `${date.getTime()} : ${req.method} : "${
+      my_url.pathname
+    }" : User Logged\n`;
 
     fs.appendFile("./http_logs.txt", log, (err, data) => {
       switch (my_url.pathname) {
         case "/":
-          console.log("Welcome Home!\n");
-          res.end("HomePage");
+          if (req.method === "GET") res.end("HomePage");
           break;
         case "/about":
           const user = my_url.query.user;
-          console.log(
-            `Hello this is about page and I'm ${user.toString()}\n`
-          );
           res.end(`Hey! Hi I am ${user.toString()}`);
           break;
         case "/search":
-          console.log("Hello this is about page\n");
-          res.end("Hey! Hi I am Chander Parkash");
+          const search = my_url.query.search_query;
+          res.end("Here are your results for => " + search);
+          break;
+        case "/signup":
+          if (req.method === "GET") res.end("This is signup form!");
           break;
         default:
-          console.log("NOT FOUND\n");
           res.end("NOT FOUND - 404");
           break;
       }
