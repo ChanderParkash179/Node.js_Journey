aedde65783f28b639ca2d3ad371a43d8f2dfef72
Chander Parkash <chanderparkash179@gmail.com>
2023-12-12 20:09:22 +0500

    handling URL's

--- modules/http/api.js
+++ modules/http/api.js
@@ -1,14 +1,36 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 http
   .createServer((req, res) => {
     let date = new Date();
-    const log = `${date.getHours().toString()}:${date.getMinutes().toString()}:${date.getSeconds().toString()}:${date.getMilliseconds().toString()} : User Logged In\n`;
+    let my_url = url.parse(req.url, true);
+
+    const log = `${date.getTime()} : "${my_url.pathname}" : User Logged\n`;
 
     fs.appendFile("./http_logs.txt", log, (err, data) => {
-      console.log("Comeback again! Thanks for using it\n");
-      res.end("Comeback again! Thanks for using it");
+      switch (my_url.pathname) {
+        case "/":
+          console.log("Welcome Home!\n");
+          res.end("HomePage");
+          break;
+        case "/about":
+          const user = my_url.query.user;
+          console.log(
+            `Hello this is about page and I'm ${user.toString()}\n`
+          );
+          res.end(`Hey! Hi I am ${user.toString()}`);
+          break;
+        case "/search":
+          console.log("Hello this is about page\n");
+          res.end("Hey! Hi I am Chander Parkash");
+          break;
+        default:
+          console.log("NOT FOUND\n");
+          res.end("NOT FOUND - 404");
+          break;
+      }
     });
   })
   .listen(8000, () => {
